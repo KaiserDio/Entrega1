cb30f23435fe2c6eb2af7672cc4b51386d1737e4
KaiserDio <daniel.kaiser20@gmail.com>
2022-04-06 12:47:56 -0300

    Se crean models y la primera plantilla html

--- Entrega1/AppGestion/models.py
+++ Entrega1/AppGestion/models.py
@@ -3,27 +3,30 @@ from django.db import models
 # Create your models here.
 
 class Articulo(models.Model):
-    nombre=models.CharField(max_lenght=40)
-    descripcion=models.CharField(max_lenght=40)
+
+    nombre=models.CharField(max_length=40)
+    descripcion=models.CharField(max_length=40)
     precio=models.IntegerField()
     cantidad=models.IntegerField()
-    
 
+    def __str__(self):
+        return (f"nombre: {self.nombre}, descripcion: {self.descripcion}, descripcion: {self.precio}, descripcion: {self.cantidad}")
+    
 class Cliente(models.Model):
-    nombre=models.CharField(max_lenght=40)
+    nombre=models.CharField(max_length=40)
     apellido=models.IntegerField()
     telefono=models.IntegerField()
-    direccion=models.CharField(max_lenght=40)
+    direccion=models.CharField(max_length=40)
 
-class Venta(models.Model):
-    idCliente=models.IntegerField()
-    idArticulo=models.IntegerField()
-    cantidad=models.IntegerField()
-    precioTotal=models.IntegerField()
+    def __str__(self):
+        return (f"nombre: {self.nombre}, descripcion: {self.apellido}, descripcion: {self.telefono}, descripcion: {self.direccion}")
 
 
 class Vendedor(models.Model):
-    nombre=models.CharField(max_lenght=40)
+    nombre=models.CharField(max_length=40)
     apellido=models.IntegerField()
     sueldo=models.IntegerField()
+
+    def __str__(self):
+        return (f"nombre: {self.nombre}, descripcion: {self.apellido}, descripcion: {self.sueldo}")
     
